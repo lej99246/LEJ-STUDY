35d8dda532d60939d05b1f731919e85ea86991b5
lej99246 <lej99246@gmail.com>
2019-10-31 11:27:16 +0900

    delete

--- workspace/dj_web/dj_web/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-
-def first(request):
-    return HttpResponse('First Page Resonpse')
-
-def second(request):
-    return HttpResponse('Second Page Response')
-
